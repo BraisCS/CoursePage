c75dcd4f35aa7070631577f8de19fe989bf3695d
BraisCS <braiscs97@gmail.com>
2024-04-17 05:36:24 +0200

    Changes in nav

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -11,23 +11,49 @@ export default function Nav({ session }: { session: any }) {
 
 
     return (
-        <nav className="h-16 w-[90%] border-b border-Light-Orange flex flex-row justify-between items-center">
-        {showButtons && (
-            <>
-                <div className="absolute bg-Dark  w-full h-full top-0 left-0 z-10 "></div>
-                <div className="absolute w-[90%] bg-Dark flex flex-row justify-between items-start z-50">
+        <>
+        {/* Pantallas grandes */}
+        <nav className="h-16 w-[90%] border-b border-Light-Orange flex flex-row justify-between items-center lg:block">
+        <div className='flex flex-row justify-between items-center h-full w-full'>
+            <p className="text-white text-xs">LOGOss</p>
+            <div className='flex flex-row justify-end items-center h-full w-full gap-5'>
+                {session ? <LogOut /> : null}
+                {session ? <a href="/upload" className="block font-DMSans text-[#ffffff] text-lg font-normal">Subir Curso</a> : null}
+                <a href="/blog" className="block font-DMSans text-[#ffffff] text-lg font-normal">Blog</a>
+                {session ? <a href="/blog" className="block font-DMSans text-[#ffffff] text-lg font-normal">Mis Cursos</a> : null}
                 {!session ? 
                 <>
-                    <a href='/register' className="fixed h-14 w-[90%] flex flex-row justify-center items-center bottom-[4.5rem] ml-0 bg-Light-Orange cursor-pointer">
-                        <p className="font-DMSans text-[#ffffff] text-lg font-normal">Únete de forma gratuita</p>
+                    <a href='/register' className="font-DMSans text-[#ffffff] text-lg font-normal">
+                    Regístrate
                     </a>
-                    <a href='/login' className="fixed h-14 w-[90%] flex flex-row justify-center items-center bottom-2 border-2 border-Light-Orange bg-Dark cursor-pointer">
-                        <p className="font-DMSans text-Light-Orange text-lg font-normal">Inicia Sesión</p>
+                    <a href='/login' className="font-DMSans text-[#ffffff] text-lg font-normal">
+                    Inicia Sesión
                     </a>
-                </> : null }
+                    </>
+                : null }
+            </div>
+            
+            </div> 
+        </nav>
+
+        {/* Pantallas pequeñas */}
+        <nav className="h-16 w-[90%] border-b border-Light-Orange flex flex-row justify-between items-center lg:hidden">
+        {showButtons && (
+            <>
+                <div className="absolute bg-Dark w-full h-full top-0 left-0 z-10 "></div>
+                <div className="absolute w-[90%] bg-Dark flex flex-row justify-between items-center z-50">
+                    {!session ? 
+                    <>
+                        <a href='/register' className="fixed h-14 w-[90%] flex flex-row justify-center items-center bottom-[4.5rem] ml-0 bg-Light-Orange cursor-pointer">
+                            <p className="font-DMSans text-[#ffffff] text-lg font-normal">Únete de forma gratuita</p>
+                        </a>
+                        <a href='/login' className="fixed h-14 w-[90%] flex flex-row justify-center items-center bottom-2 border-2 border-Light-Orange bg-Dark cursor-pointer">
+                            <p className="font-DMSans text-Light-Orange text-lg font-normal">Inicia Sesión</p>
+                        </a>
+                    </> : null }
                     <div className="w-[100%] flex flex-col justify-start items-start mt-32 relative"> {/* Asegúrate de que este contenedor sea relativo */}
                         {session ? <LogOut /> : null}
-                        <a href="/upload" className="block font-DMSans text-[#ffffff] text-lg font-normal">Subir Curso</a>
+                        {session ? <a href="/upload" className="block font-DMSans text-[#ffffff] text-lg font-normal">Subir Curso</a> : null}
                         <a href="/blog" className="block font-DMSans text-[#ffffff] text-lg font-normal">Blog</a>
                         {session ? <a href="/blog" className="block font-DMSans text-[#ffffff] text-lg font-normal">Mis Cursos</a> : null}
                         <div className='w-full flex flex-row justify-between items-start'>
@@ -44,12 +70,13 @@ export default function Nav({ session }: { session: any }) {
                         )}
                     <Search/>
                     </div>
-                    <Image onClick={() => setShowButtons(!showButtons)} className="cursor-pointer mt-[5.8rem] h-9 w-9" src={"/cross.svg"} alt="Icono de cierre" height={30} width={30} />
+                    <Image onClick={() => setShowButtons(!showButtons)} className="cursor-pointer h-9 w-9" src={"/cross.svg"} alt="Icono de cierre" height={30} width={30} />
                 </div>
             </>
         )}
         <p className="text-white text-xs">LOGOss</p> 
         <Image onClick={() => setShowButtons(!showButtons)} className="cursor-pointer h-9 w-9" src={"/menu.svg"} alt="Icono de menú" height={30} width={30} />
         </nav>
+    </>
     );
 }
