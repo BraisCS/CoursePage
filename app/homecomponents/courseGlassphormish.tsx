8a3a9c789a4b5849e1e1f8bc6ff3bc996a426371
BraisCS <braiscs97@gmail.com>
2024-05-25 14:47:14 +0200

    Slider Home Done

--- app/homecomponents/courseGlassphormish.tsx
+++ app/homecomponents/courseGlassphormish.tsx
@@ -1,7 +1,10 @@
 "use client";
 import { useEffect, useRef } from 'react';
 import Swiper from 'swiper/bundle';
+import { Swiper as SwiperType } from "swiper/types"; 
 import 'swiper/swiper-bundle.css';
+import "./courseGlassphormish.css";
+import Image from 'next/image';
 
 interface Requirement {
   id: number;
@@ -32,7 +35,7 @@ interface Course {
 }
 
 export default function CourseGlassphormish({ courses }: { courses: Course[] }) {
-  const swiperRef = useRef<Swiper | null>(null);
+  const swiperRef = useRef<SwiperType | null>(null);
 
   useEffect(() => {
     swiperRef.current = new Swiper('.swiper-container-first', {
@@ -40,29 +43,56 @@ export default function CourseGlassphormish({ courses }: { courses: Course[] }) 
       spaceBetween: 10,
       initialSlide: 0,
       speed: 1000,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        type: 'custom',
+        renderCustom: function (swiper: SwiperType, current: number, total: number) {
+          const currentFormatted = current < 10 ? '0' + current : current;
+          const totalFormatted = total < 10 ? '0' + total : total;
+          return `<span class="swiper-pagination-current">${currentFormatted}</span>
+                  <span class="swiper-pagination-separator"> _____ </span>
+                  <span class="swiper-pagination-total">${totalFormatted}</span>`;
+        }
+      },
     });
   }, []);
 
   return (
-    <div className='swiper-container-first overflow-hidden'>
-      <div className='swiper-wrapper flex flex-row'>
-        {courses.map((course, index) => (
-          <div key={index} className="swiper-slide flex flex-col justify-between items-start p-5 min-h-[28rem] min-w-[2rem] rounded-lg border border-Light-Orange bg-[#ffffff30]">
-            <div className='w-full flex flex-col justify-start items-start gap-5'>
-              <div className='flex flex-col justify-start items-start gap-2'>
-                <p className="text-[#fff] font-DMSans font-light text-2xl">{course.title}</p>
-                <p className="text-[#fff] font-DMSans font-extralight text-sm">10 semanas | 2 horas por semana</p>
+    <div className='swiper-container-wrapper h-full relative overflow-hidden'>
+      <div className='swiper-container-first flex flex-col'>
+        <div className='swiper-wrapper flex flex-row'>
+          {courses.map((course, index) => (
+            <div key={index} className="swiper-slide flex flex-col justify-between items-start p-5 min-h-[28rem] min-w-[2rem] rounded-lg border border-Light-Orange ">
+              <div className='w-full flex flex-col justify-start items-start gap-5'>
+                <div className='flex flex-col justify-start items-start gap-2'>
+                  <p className="text-[#fff] font-DMSans font-light text-2xl">{course.title}</p>
+                  <p className="text-[#fff] font-DMSans font-extralight text-sm">10 semanas | 2 horas por semana</p>
+                </div>
+                <p className="text-[#fff] font-DMSans font-extralight text-base">{course.introduction}</p>
               </div>
-              <p className="text-[#fff] font-DMSans font-extralight text-base">{course.introduction}</p>
-            </div>
-            <div className='flex flex-col justify-start items-start gap-2'>
-              <p className="text-[#fff] font-DMSans font-extralight text-4xl">{course.price} €</p>
-              <div className='w-full flex flex-row justify-start items-center'>
-                <a href={`courses/${course.id}`} className="text-[#fff] font-DMSans font-normal text-start py-2 px-6 rounded-lg border border-Light-Orange cursor-pointer">Ver curso</a>
+              <div className='flex flex-col justify-start items-start gap-2 w-full'>
+                <p className="text-[#fff] font-DMSans font-extralight text-4xl">{course.price} €</p>
+                <div className='w-full flex flex-row justify-between items-center'>
+                  <a href={`courses/${course.id}`} className=" w-[80%] text-[#fff] font-DMSans font-normal text-start py-2 px-6 rounded-lg border border-Light-Orange cursor-pointer">Ver curso</a>
+                  <Image height={40} width={40} alt='Icono Bookmark' src={"/bookmark.svg"} />
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
+      </div>
+      <div className="swiper-controls w-full absolute bottom-6 flex flex-row justify-between items-center p-4">
+        <div className="flex flex-row justify-start items-center">
+          <div className="swiper-button-prev"></div>
+          <div className="swiper-button-next mr-[85%]"></div>
+        </div>
+        <div className='flex justify-end w-full'>
+          <div className="swiper-pagination ml-[40%]"></div>
+        </div>
       </div>
     </div>
   );
--- app/homecomponents/main.tsx
+++ app/homecomponents/main.tsx
@@ -34,12 +34,12 @@ export default function Main({coursesWithImages} : { coursesWithImages: Course[]
     <>
       <main className={`w-[95%] lg:w-[95%] ml-[5%] flex flex-row justify-start py-[5%] items-start`}>
         <div className="w-[45%] flex flex-col justify-start items-start gap-3">
-          <h1 className=" text-[#f6f6f6] font-DMSans font-normal text-5xl"> Todos los cursos de <br /> CATEGORIA</h1>
+          <h1 className=" text-[#f6f6f6] font-DMSans font-normal text-5xl"> Todos los cursos de <br /> Desarrollo Web</h1>
           <p className=" text-[#f6f6f6] font-DMSans font-normal text-2xl">  Lorem ipsum dolor sit amet consectetur</p>
           <p className=" text-[#f6f6f6] font-DMSans font-extralight text-2xl">Lorem ipsum dolor sit amet consectetur adipiscing elit maecenas congue felis, aenean montes per proin sollicitudin rhoncus vel condimentum iaculis eget, erat platea sociis sed porttitor litora quisque orci tortor.</p>
           <p className=" text-[#eeb462] font-DMSans font-extralight text-lg">  Lorem ipsum dolor sit amet consectetur</p>
         </div>
-        <div className="flex flex-row justify-start items-start gap-10 h-[28rem] w-[55%] p-2">
+        <div className="flex flex-row justify-start items-start gap-10 h-[34rem] w-[55%] p-2">
           <CourseGlassphormish courses={coursesWithImages} />
         </div>
       </main>
